38e28a7f171de3bdaafa5e77fe457990967bf90a
sv.giampa <sv.giampa@gmail.com>
2024-02-14 16:59:00 +0100

    README and License
    * Applied License to source files
    * Added minimal README information

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # Objackson
-A JSON serializer for Python based on json module
+This is a JSON serializer for Python that supports deep serialization of objects and full reconstruction of object structure on deserialization.
+
+At the moment it does not support circular references.
+
+# How to use
+
+
+    from objackson import obj2json, json2obj
+
+    class MyClass:
+
+        def __init__(self, x=0, y=0):
+            self.__x = x
+            self.__y = y
+        
+        @property
+        def x(self):
+            return self.__x
+
+        @property
+        def y(self):
+            return self.__y
+
+        def __eq__(self, obj)
+            return self.x == obj.x and self.y = obj.y
+        
+    def main():
+        obj1 = TestClass(2, 3)
+        json = obj2json(obj1)
+        obj2 = json2obj(json)
+
+        # obj2 is a copy of obj1. Test it:
+        if obj1 == obj2:
+            print("SUCCESS")
+        else:
+            print("FAIL")
+
+    if __name__ == "__main__"
+        main()
+
+
+For a class to be serializable, it must have a constructor that allows 0-arguments initialization.
--- test/test_serialize.py
+++ test/test_serialize.py
@@ -1,3 +1,17 @@
+#    Copyright 2024 Salvatore Giampà
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
 from objackson import obj2json, json2obj, JSONObject
 from unittest import TestCase
 
@@ -9,7 +23,8 @@ class TestClass:
     def __eq__(self, __value: object) -> bool:
         __value: TestClass
         return self.x == __value.x and self.y == __value.y
-class ObjacksonTest(TestCase):
+    
+class TestSerialize(TestCase):
     
     def test_obj2json(self):
         obj1 = TestClass()
@@ -39,3 +54,18 @@ class ObjacksonTest(TestCase):
             TestClass(3,5),
             test_obj
         )
+        
+    
+    def test_circular(self):
+        class Ref:
+            def __init__(self):
+                self.x = None
+        
+        ref1 = Ref()
+        ref2 = Ref()
+        ref1.x = ref2
+        ref2.x = ref1
+        
+        json = obj2json(ref1)
+        
+        obj = json2obj(json)
